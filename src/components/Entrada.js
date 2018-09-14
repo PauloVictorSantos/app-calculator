9b82254a8bc8165ef770729dd6d1a24b51b20bc1
paulovictorln <paulovictor494@gmail.com>
2018-09-14 15:59:18 -0300

    entrada

--- src/components/Entrada.js
+++ src/components/Entrada.js
@@ -1,6 +1,17 @@
 import React from 'react';
-import {Text} from 'react-native';
+import { View } from 'react-native';
+import Numero from './Numero';
 
-export default props=>(
-    <Text>Entrada</Text>
+export default props => (
+    <View>
+        <Numero style={styles.numeros}/>
+        <Numero style={styles.numeros}/>
+    </View>
 );
+
+const styles = StyleSheet.create({
+    numeros: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    }
+});
